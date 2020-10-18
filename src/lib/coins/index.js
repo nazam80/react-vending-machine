280f7ac6b66f09454390c112c2cc8c8d6510dabe
Antonio Martínez López <nazam80@gmail.com>
2020-10-18 16:33:39 +0200

    Improved calculate change with limited number of coins

--- src/lib/coins/index.js
+++ src/lib/coins/index.js
@@ -1,44 +1,64 @@
 export const CoinsValues = {
     TWO_EURO: {
         label: '2 euros',
-        cents: 200,        
+        cents: 200,
+        units: 5,
     },
     ONE_EURO: {
         label: '1 euro',
-        cents: 100,        
+        cents: 100,
+        units: 5,
     },
     FIFTY_CENTS: {
         label: '50 cents',
-        cents: 50,        
+        cents: 50,
+        units: 5,
     },
     TWENTY_CENTS: {
         label: '20 cents',
-        cents: 20,        
+        cents: 20,
+        units: 5,
     },
     TEN_CENTS: {
         label: '10 cents',
-        cents: 10,        
+        cents: 10,
+        units: 5,
     },
     FIVE_CENTS: {
         label: '5 cents',
-        cents: 5,        
+        cents: 5,
+        units: 5,
     },
 }
 
 export const getCoins = () => [CoinsValues.TWO_EURO, CoinsValues.ONE_EURO, CoinsValues.FIFTY_CENTS, CoinsValues.TWENTY_CENTS, CoinsValues.TEN_CENTS, CoinsValues.FIVE_CENTS]
 
+export const getCoinWithUnits = (coin, units) => {
+    return {
+        ...coin,
+        units: units,
+    }
+}
+
+export const calculateChange = (valueToChangeInCents, coins) => {
+    const getUnits = (value, coin) => {
+        const unitNeeded = Math.floor(value / coin.cents)
+        return Math.min(unitNeeded, coin.units)
+    }
+    const getNextValueToChange = (currentValueToChange, coinValue, units) => currentValueToChange - (coinValue * units)
 
 
-export const calculateChange = (valueInCents, coins) => {
-    const sortedCoins = coins.sort((a,b) => b.cents-a.cents) 
-    
+
+    const sortedCoins = coins.sort((a, b) => b.cents - a.cents)
+
     const change = []
-    for (let j=0; j<sortedCoins.length && valueInCents>0; j++) {        
-        change.push( {
-            ...sortedCoins[j],
-            units: Math.floor(valueInCents / sortedCoins[j].cents),
-        })
-        valueInCents = valueInCents % sortedCoins[j].cents
+    for (let j = 0; j < sortedCoins.length && valueToChangeInCents > 0; j++) {
+        const currentCoin = sortedCoins[j]
+        const unitsOfCurrentCoin = getUnits(valueToChangeInCents, currentCoin)
+        if (unitsOfCurrentCoin > 0) {
+            change.push(getCoinWithUnits(currentCoin, unitsOfCurrentCoin))
+        }
+        valueToChangeInCents = getNextValueToChange(valueToChangeInCents, currentCoin.cents, unitsOfCurrentCoin)
     }
     return change;
 
--- src/lib/coins/index.test.js
+++ src/lib/coins/index.test.js
@@ -1,21 +1,21 @@
-import {CoinsValues, getCoins, calculateChange} from '.'
+import {CoinsValues, getCoins, getCoinWithUnits, calculateChange} from '.'
 
 describe('Given an array of coins values', () => {
     it('when we calculate change for 6 euro returns 3 coins of 2 euro', () => {
       const expected = [
-          {...CoinsValues.TWO_EURO, units: 3},
+          getCoinWithUnits(CoinsValues.TWO_EURO, 3),
         ]
-  
+      
       const result = calculateChange(600, getCoins())
-  
+      
       expect(result).toEqual(expected)
       
     })
   
     it('when we calculate change for 7 euro returns 3 coins of 2 euro and 1 coin of 1 euro', () => {
-        const expected = [
-            {...CoinsValues.TWO_EURO, units: 3},
-            {...CoinsValues.ONE_EURO, units: 1},
+        const expected = [ 
+          getCoinWithUnits(CoinsValues.TWO_EURO, 3),
+          getCoinWithUnits(CoinsValues.ONE_EURO, 1),
         ]
     
         const result = calculateChange(700, getCoins())
@@ -27,9 +27,9 @@ describe('Given an array of coins values', () => {
       
       it('when we calculate change for 750 cents returns 3 coins of 2 euro, 1 coin of 1 euro, 1 coin of 50 cents', () => {
         const expected = [
-            {...CoinsValues.TWO_EURO, units: 3},
-            {...CoinsValues.ONE_EURO, units: 1},
-            {...CoinsValues.FIFTY_CENTS, units: 1},
+            getCoinWithUnits(CoinsValues.TWO_EURO, 3),
+            getCoinWithUnits(CoinsValues.ONE_EURO, 1),
+            getCoinWithUnits(CoinsValues.FIFTY_CENTS, 1),
         ]
     
         const result = calculateChange(750, getCoins())
@@ -40,10 +40,10 @@ describe('Given an array of coins values', () => {
 
       it('when we calculate change for 770 cents returns 3 coins of 2 euro, 1 coin of 1 euro, 1 coin of 50 cents, 1 coin of 20 cents', () => {
         const expected = [
-            {...CoinsValues.TWO_EURO, units: 3},
-            {...CoinsValues.ONE_EURO, units: 1},
-            {...CoinsValues.FIFTY_CENTS, units: 1},
-            {...CoinsValues.TWENTY_CENTS, units: 1},
+            getCoinWithUnits(CoinsValues.TWO_EURO, 3),
+            getCoinWithUnits(CoinsValues.ONE_EURO, 1),
+            getCoinWithUnits(CoinsValues.FIFTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TWENTY_CENTS, 1),
         ]
     
         const result = calculateChange(770, getCoins())
@@ -54,11 +54,11 @@ describe('Given an array of coins values', () => {
 
       it('when we calculate change for 780 cents returns 3 coins of 2 euro, 1 coin of 1 euro, 1 coin of 50 cents, 1 coin of 20 cents, 1 coin of 10 cents', () => {
         const expected = [
-            {...CoinsValues.TWO_EURO, units: 3},
-            {...CoinsValues.ONE_EURO, units: 1},
-            {...CoinsValues.FIFTY_CENTS, units: 1},
-            {...CoinsValues.TWENTY_CENTS, units: 1},
-            {...CoinsValues.TEN_CENTS, units: 1},
+            getCoinWithUnits(CoinsValues.TWO_EURO, 3),
+            getCoinWithUnits(CoinsValues.ONE_EURO, 1),
+            getCoinWithUnits(CoinsValues.FIFTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TWENTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TEN_CENTS, 1),
         ]
     
         const result = calculateChange(780, getCoins())
@@ -70,12 +70,12 @@ describe('Given an array of coins values', () => {
 
       it('when we calculate change for 785 cents returns 3 coins of 2 euro, 1 coin of 1 euro, 1 coin of 50 cents, 1 coin of 20 cents, 1 coin of 10 cents, 1 coin of 5 cents', () => {
         const expected = [
-            {...CoinsValues.TWO_EURO, units: 3},
-            {...CoinsValues.ONE_EURO, units: 1},
-            {...CoinsValues.FIFTY_CENTS, units: 1},
-            {...CoinsValues.TWENTY_CENTS, units: 1},
-            {...CoinsValues.TEN_CENTS, units: 1},
-            {...CoinsValues.FIVE_CENTS, units: 1},
+            getCoinWithUnits(CoinsValues.TWO_EURO, 3),
+            getCoinWithUnits(CoinsValues.ONE_EURO, 1),
+            getCoinWithUnits(CoinsValues.FIFTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TWENTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TEN_CENTS, 1),
+            getCoinWithUnits(CoinsValues.FIVE_CENTS, 1),
         ]
     
         const result = calculateChange(785, getCoins())
@@ -83,6 +83,52 @@ describe('Given an array of coins values', () => {
         expect(result).toEqual(expected)
         
       })
+
+
+      it('when we calculate change for 285 cents without coins of 2 euro returns 2 coin of 1 euro, 1 coin of 50 cents, 1 coin of 20 cents, 1 coin of 10 cents, 1 coin of 5 cents', () => {
+        const coins =[getCoinWithUnits(CoinsValues.TWO_EURO, 0), CoinsValues.ONE_EURO, CoinsValues.FIFTY_CENTS, CoinsValues.TWENTY_CENTS, CoinsValues.TEN_CENTS, CoinsValues.FIVE_CENTS]
+        
+        const expected = [           
+            getCoinWithUnits(CoinsValues.ONE_EURO, 2),
+            getCoinWithUnits(CoinsValues.FIFTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TWENTY_CENTS, 1),
+            getCoinWithUnits(CoinsValues.TEN_CENTS, 1),
+            getCoinWithUnits(CoinsValues.FIVE_CENTS, 1),
+        ]
+    
+        const result = calculateChange(285, coins)
+    
+        expect(result).toEqual(expected)
+        
+      })
+
+      it('when we calculate change for 130 cents without coins of 1 euro and 50 cents returns 6 coin of 20 cents, 1 coin of 10 cents', () => {
+        const coins = [CoinsValues.TWO_EURO, getCoinWithUnits(CoinsValues.ONE_EURO,0), getCoinWithUnits(CoinsValues.FIFTY_CENTS, 0), CoinsValues.TWENTY_CENTS, CoinsValues.TEN_CENTS, CoinsValues.FIVE_CENTS]
+         
+        const expected = [                       
+            getCoinWithUnits(CoinsValues.TWENTY_CENTS, 5), //default max units are 5
+            getCoinWithUnits(CoinsValues.TEN_CENTS, 3),            
+        ]
+    
+        const result = calculateChange(130, coins)
+    
+        expect(result).toEqual(expected)
+        
+      })
   
+      it('when we calculate change for 130 cents without coins of 1 euro and 20 cents returns 2 coin of 50 cents, 3 coins of 10 cents', () => {
+        const coins = [CoinsValues.TWO_EURO,  getCoinWithUnits(CoinsValues.ONE_EURO, 0), CoinsValues.FIFTY_CENTS, getCoinWithUnits(CoinsValues.TWENTY_CENTS, 0), CoinsValues.TEN_CENTS, CoinsValues.FIVE_CENTS]
+        
+        const expected = [      
+            getCoinWithUnits(CoinsValues.FIFTY_CENTS, 2),                 
+            getCoinWithUnits(CoinsValues.TEN_CENTS, 3),
+        ]
+    
+        const result = calculateChange(130, coins)
+    
+        expect(result).toEqual(expected)
+        
+      })
+
   })
   
